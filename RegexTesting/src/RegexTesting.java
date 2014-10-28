b39a780e7faf1d22cc27ff2c6c43fd88d8a8419a
Tyler <Tyler@10.104.177.115>
2014-10-28 10:39:06 -0700

    Added in file/user input capabilities.

--- RegexTesting/src/RegexTesting.java
+++ RegexTesting/src/RegexTesting.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 // Tyler Herrin
 
 
@@ -5,7 +9,28 @@ public class RegexTesting {
 
 	public static void main(String[] args) 
 	{
-
+		Scanner in;
+		
+		if(args.length >= 1)
+		{
+			System.out.println(args[0]);
+			File input = new File(args[0]);
+			try 
+			{
+				in = new Scanner(input);
+			} 
+			catch (FileNotFoundException e)
+			{
+				System.out.println("File not found. Reverting to user input.");
+				in = new Scanner(System.in);
+			}
+		}
+		else
+			in = new Scanner(System.in);
+		
+		
+		
+		in.close();
 	}
 
 }
