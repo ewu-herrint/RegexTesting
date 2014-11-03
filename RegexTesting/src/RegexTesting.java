14b87d99a0ad61d65fa4592dac3666b8feaf5be3
Tyler <Tyler@10.104.177.115>
2014-11-03 10:47:54 -0800

    Implemented more functions.

--- RegexTesting/src/RegexTesting.java
+++ RegexTesting/src/RegexTesting.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 // Tyler Herrin
@@ -127,14 +128,22 @@ public class RegexTesting {
 
 	private static void timeCheck(Scanner in) 
 	{
-		// TODO Auto-generated method stub
+		String input = in.nextLine();
+		if(input.matches("^([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$"))
+		{
+			System.out.println("Valid Time.");
+		}
+		else
+		{
+			System.out.println("Invalid Time.");
+		}
 		
 	}
 
 	private static void dateCheck(Scanner in) 
 	{
 		String input = in.nextLine();
-		if(input.matches("^(0[1-9]|1[012])([-/])(0[1-9]|[12][0-9]|3[01])\\2(19|20)\\d\\d"))
+		if(input.matches("^(0[1-9]|1[012])([-/])(0[1-9]|[12][0-9]|3[01])\\2(19|20)\\d\\d$"))
 		{
 			System.out.println("Valid Date.");
 		}
@@ -147,14 +156,36 @@ public class RegexTesting {
 
 	private static void cityCheck(Scanner in) 
 	{
-		// TODO Auto-generated method stub
+		String[] states = {"AL","AK","AZ","AR","CA","CO","CT","DE","FL",
+		                   "GA","HI","ID","IL","IN","IA","KS","KY","LA",
+		                   "ME","MD","MA","MI","MN","MS","MO","MT","NE",
+		                   "NV","NH","NJ","NM","NY","NC","ND","OH","OK",
+		                   "OR","PA","RI","SC","SD","TN","TX","UT","VT",
+		                   "VA","WA","WV","WI","WY"};
 		
+		String input = in.nextLine();
+		int stateLoc = input.indexOf(",") + 1;
+		String state = input.substring(stateLoc, stateLoc+2);
+		if(Arrays.binarySearch(states, state) >= 0 && stateLoc >= 0)
+		{
+			if(input.matches("^[A-Za-z]+, [A-Z]{2} [0-9]{5}$"))
+			{
+				System.out.println("Valid City/State/ZIP.");
+			}
+			else
+			{
+				System.out.println("Invalid City/State/ZIP.");
+			}
+		}
+		else
+		{
+			System.out.println("Invalid City/State/ZIP.");
+		}
 	}
 
 	private static void addressCheck(Scanner in) 
 	{
-		// TODO Auto-generated method stub
-		// Need to implement ability to do "1st St" etc.
+		// TODO Need to implement ability to do "1st St" etc.
 		String input = in.nextLine();
 		if(input.matches("^[0-9]{1, 5}, [A-Z][a-z]+ ((St.|Rd.|Ave.|Blvd.)|(Street|Road|Avenue|Boulevard))$"))
 		{
@@ -181,8 +212,15 @@ public class RegexTesting {
 
 	private static void emailCheck(Scanner in) 
 	{
-		// TODO Auto-generated method stub
-		
+		String input = in.nextLine();
+		if(input.matches("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$"))
+		{
+			System.out.println("Valid E-mail.");
+		}
+		else
+		{
+			System.out.println("Invalid E-mail.");
+		}
 	}
 
 	private static void phoneNumberCheck(Scanner in) 
